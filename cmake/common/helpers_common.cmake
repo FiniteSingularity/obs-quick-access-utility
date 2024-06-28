074d5b57fd9cb1a57687552b8268de4ec1073532
FiniteSingularity <finitesingularityttv@gmail.com>
2024-06-28 15:43:01 -0500

    working in-tree builds.

--- cmake/common/helpers_common.cmake
+++ cmake/common/helpers_common.cmake
@@ -41,18 +41,3 @@ function(check_uuid uuid_string return_value)
       PARENT_SCOPE)
 endfunction()
 
-if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/src/plugin-support.c.in")
-  configure_file(src/plugin-support.c.in plugin-support.c @ONLY)
-  add_library(plugin-support STATIC)
-  target_sources(
-    plugin-support
-    PRIVATE plugin-support.c
-    PUBLIC src/plugin-support.h)
-  target_include_directories(plugin-support PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/src")
-  if(OS_LINUX
-     OR OS_FREEBSD
-     OR OS_OPENBSD)
-    # add fPIC on Linux to prevent shared object errors
-    set_property(TARGET plugin-support PROPERTY POSITION_INDEPENDENT_CODE ON)
-  endif()
-endif()
